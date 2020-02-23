b65ee52a2f2047697312646ee2428fd34277594e
cococcococ <60924502+cococcococ@users.noreply.github.com>
2020-02-23 23:50:07 +0900

    Update BLE_midi.ino
    SoundCortex sounds/off when it recieve BLE MIDI keyboad sent note on/off. only single note.

--- BLE_midi.ino
+++ BLE_midi.ino
@@ -1,27 +1,28 @@
 #include <BLEDevice.h>
-#include <BLEServer.h>
 #include <BLEUtils.h>
-#include <BLE2902.h>
+#include <BLEServer.h>
 
-uint8_t note = 38;
-int SNARE[6] = {150, 4000, 38, 3, 0, 0}; //{threshold, sensitivity, note(no use), flag, velocity, last peakValue}
-boolean snareFlag = false;
-
-const int PIN_ANALOG_INPUT = 1;
-
-BLECharacteristic *pCharacteristic;
-bool deviceConnected = false;
+#include <PSG810.h>
+PSG810* psg;
 
 #define MIDI_SERVICE_UUID        "03b80e5a-ede8-4b33-a751-6ce34ec4c700"
 #define MIDI_CHARACTERISTIC_UUID "7772e5db-3868-4112-a1a9-f2669d106bf3"
+#define DEVIVE_NAME "MIDIduino"
 
-uint8_t midiPacket[] = {
-  0x80,  // header
-  0x80,  // timestamp, not implemented
-  0x00,  // status
-  0x3c,  // 0x3c == 60 == middle c
-  0x00   // velocity
-};
+#define buttonPin 18 //esp32 =>arduno 13pin
+#define LED 2 //Soldered LED[IO2]
+
+int buttonState = 0; // variable for reading the pushbutton status
+int scale;
+int note_name;
+
+BLEServer *pServer;
+BLEAdvertising *pAdvertising;
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+
+int pos = 0;
+char midi[5];
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -33,71 +34,123 @@ class MyServerCallbacks: public BLEServerCallbacks {
     }
 };
 
-#define LED 2 //Soldered LED[IO2]
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+      pos = 0;
+
+      if (rxValue.length() > 0) {
+        for (int i = 0; i < rxValue.length(); i++)
+        {
+          midi[pos] = rxValue[i];
+          pos++;
+          if(pos == 5)
+          {
+            Serial.print(midi[2],HEX);
+            Serial.print(" ");
+            Serial.print(midi[3],HEX);
+            Serial.print(" ");
+            Serial.println(midi[4],HEX);
+            Serial.println();
+            pos = 0;
+            
+            switch (midi[2]) {
+              case 0x90:
+                // Note on message
+                note_name = midi[3] % 12;
+                scale = (midi[3] / 12) + 1;
+//                Serial.print(scale);
+//                Serial.print(" ");
+//                Serial.println(note_name);
+//                Serial.println();
+                switch (note_name) {
+                  case 0:
+                    psg->SetFrequency(PSG::PSG_A, CalcFreqByMML(scale, 'C'));
+                    break;
+                  case 1:
+                    psg->SetFrequency(PSG::PSG_A, CalcFreqByMML(scale, 'C', true));
+                    break;
+                  case 2:
+                    psg->SetFrequency(PSG::PSG_A, CalcFreqByMML(scale, 'D'));
+                    break;
+                  case 3:
+                    psg->SetFrequency(PSG::PSG_A, CalcFreqByMML(scale, 'D', true));
+                    break;
+                  case 4:
+                    psg->SetFrequency(PSG::PSG_A, CalcFreqByMML(scale, 'E'));
+                    break;
+                  case 5:
+                    psg->SetFrequency(PSG::PSG_A, CalcFreqByMML(scale, 'F'));
+                    break;
+                  case 6:
+                    psg->SetFrequency(PSG::PSG_A, CalcFreqByMML(scale, 'F', true));
+                    break;
+                  case 7:
+                    psg->SetFrequency(PSG::PSG_A, CalcFreqByMML(scale, 'G'));
+                    break;
+                  case 8:
+                    psg->SetFrequency(PSG::PSG_A, CalcFreqByMML(scale, 'G', true));
+                    break;
+                  case 9:
+                    psg->SetFrequency(PSG::PSG_A, CalcFreqByMML(scale, 'A'));
+                    break;
+                  case 10:
+                    psg->SetFrequency(PSG::PSG_A, CalcFreqByMML(scale, 'A', true));
+                    break;
+                  case 11:
+                    psg->SetFrequency(PSG::PSG_A, CalcFreqByMML(scale, 'B'));
+                    break;
+                }
+                digitalWrite(LED, HIGH);
+                //delay(200);
+                break;
+              case 0x80:
+                // Note off message
+                digitalWrite(LED, LOW);
+                psg->SetFrequency(PSG::PSG_A, noteFreq[127]);
+                //Serial.println("Note off");
+                //delay(200);
+                break;
+            }
+          }          
+        }
+      }
+    }
+};
 
 void setup() {
+  pinMode(buttonPin, INPUT);
   pinMode(LED, OUTPUT);
   
   Serial.begin(115200);
 
-  BLEDevice::init("MIDIduino"); //Device Name
-
-  // Create the BLE Server
-  BLEServer *pServer = BLEDevice::createServer();
+  BLEDevice::init(DEVIVE_NAME);
+  pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
-  BLEDevice::setEncryptionLevel((esp_ble_sec_act_t)ESP_LE_AUTH_REQ_SC_BOND);
-
-  // Create the BLE Service
   BLEService *pService = pServer->createService(BLEUUID(MIDI_SERVICE_UUID));
-
-  // Create a BLE Characteristic
   pCharacteristic = pService->createCharacteristic(
-                      BLEUUID(MIDI_CHARACTERISTIC_UUID),
-                      BLECharacteristic::PROPERTY_READ   |
-                      BLECharacteristic::PROPERTY_NOTIFY |
-                      BLECharacteristic::PROPERTY_WRITE_NR
-                    );
-  pCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
-
-  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
-  // Create a BLE Descriptor
-  pCharacteristic->addDescriptor(new BLE2902());
-
-  // Start the service
+    BLEUUID(MIDI_CHARACTERISTIC_UUID),
+    BLECharacteristic::PROPERTY_READ   |
+    BLECharacteristic::PROPERTY_WRITE  |
+    BLECharacteristic::PROPERTY_NOTIFY |
+    BLECharacteristic::PROPERTY_WRITE_NR
+  );
+  pCharacteristic->setCallbacks(new MyCallbacks());
   pService->start();
 
-  // Start advertising
+  BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
+  oAdvertisementData.setFlags(0x04);
+  oAdvertisementData.setCompleteServices(BLEUUID(MIDI_SERVICE_UUID));
+  oAdvertisementData.setName(DEVIVE_NAME);
+  pAdvertising = pServer->getAdvertising();
+  pAdvertising->setAdvertisementData(oAdvertisementData);
+  pAdvertising->start();
 
-  BLESecurity *pSecurity = new BLESecurity();
-  pSecurity->setAuthenticationMode(ESP_LE_AUTH_REQ_SC_BOND);
-  pSecurity->setCapability(ESP_IO_CAP_NONE);
-  pSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK);
-
-  pServer->getAdvertising()->addServiceUUID(MIDI_SERVICE_UUID);
-  pServer->getAdvertising()->start();
-
+  psg = new PSG810();
+  psg->SetMixer(0x110);
+  psg->SetVolume(PSG::PSG_A, 0x0f);
 }
 
 void loop() {
-  if (deviceConnected) {
-    // Note on message
-    midiPacket[2] = 0x90;   // Status "note on" & "MIDI Ch"
-    midiPacket[3] = 48;     // note number
-    midiPacket[4] = 127;    // velocity
-    pCharacteristic->setValue(midiPacket, 5); // packet, length in bytes
-    pCharacteristic->notify();
-    digitalWrite(LED, HIGH);
-   
-    delay(1000);
-   
-    // Note off message
-    midiPacket[2] = 0x80;   // Status "note off" & "MIDI Ch"
-    midiPacket[3] = 48;     // note number
-    midiPacket[4] = 127;    // velocity
-    pCharacteristic->setValue(midiPacket, 5); // packet, length in bytes
-    pCharacteristic->notify();
-    digitalWrite(LED, LOW);
-   
-    delay(1000);
-  }
+  delay(10);
 }
